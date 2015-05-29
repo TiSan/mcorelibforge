8f46c5d07095962aeb2b65288a1e7d5e1e2e45c4
TiSan <webmaster@dropbyte.de>
2015-05-29 17:18:25 +0200

    Updated Bukkit for the new PlayerObject

--- src/main/java/de/tisan/mcoref/helpers/Bukkit.java
+++ src/main/java/de/tisan/mcoref/helpers/Bukkit.java
@@ -9,29 +9,25 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import de.tisan.mcoref.events.core.BukkitEventManager;
 import de.tisan.mcoref.plugins.BukkitPluginManager;
 import de.tisan.mcoref.plugins.commands.BukkitCommandManager;
+import de.tisan.mcoref.plugins.players.BukkitPlayer;
 
 public class Bukkit {
 	private static BukkitPluginManager pluginManager;
 	private static BukkitCommandManager commandManager;
-	
-	public static void sendMessage(EntityPlayer player, String message) {
-		player.addChatComponentMessage(new ChatComponentText(message));
-	}
 
-	public static void sendMessage2(EntityPlayer player, String message) {
-		player.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.AQUA + message));
+	public static void sendMessage(BukkitPlayer player, String message) {
+		player.addChatComponentMessage(new ChatComponentText(message));
 	}
 
 	public static void broadcastMessage(String message) {
 		if (Bukkit.isServer()) {
-			for (EntityPlayer pl : Bukkit.getPlayers()) {
+			for (BukkitPlayer pl : Bukkit.getPlayers()) {
 				Bukkit.sendMessage(pl, message);
 			}
 		} else {
@@ -40,11 +36,11 @@ public class Bukkit {
 
 	}
 
-	public static EntityPlayer getPlayer(String name) {
+	public static BukkitPlayer getPlayer(String name) {
 		if (Bukkit.isServer()) {
 			for (Object pl : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
-				if (pl instanceof EntityPlayer) {
-					EntityPlayer p = (EntityPlayer) pl;
+				if (pl instanceof BukkitPlayer) {
+					BukkitPlayer p = (BukkitPlayer) pl;
 					if (p.getCommandSenderName().equals(name)) {
 						return p;
 					}
@@ -54,49 +50,60 @@ public class Bukkit {
 		}
 		return null;
 	}
-	
+
 	@SideOnly(Side.CLIENT)
-	public static EntityPlayer getClientPlayer() {
-		if(Bukkit.isClient()){
+	public static BukkitPlayer getClientPlayer() {
+		if (Bukkit.isClient()) {
+			return new BukkitPlayer(Minecraft.getMinecraft().thePlayer);
+		}
+		return null;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public static EntityPlayer getClientVanillaPlayer() {
+		if (Bukkit.isClient()) {
 			return Minecraft.getMinecraft().thePlayer;
 		}
 		return null;
 	}
 
-	public static ArrayList<EntityPlayer> getPlayers() {
+	@SideOnly(Side.SERVER)
+	public static ArrayList<BukkitPlayer> getPlayers() {
 		if (Bukkit.isServer()) {
-			ArrayList<EntityPlayer> players = new ArrayList<EntityPlayer>();
+			ArrayList<BukkitPlayer> players = new ArrayList<BukkitPlayer>();
 			for (Object pl : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
 				if (pl instanceof EntityPlayer) {
 					EntityPlayer p = (EntityPlayer) pl;
-					players.add(p);
+					players.add(new BukkitPlayer(p));
 				}
 			}
 			return players;
 		}
 		return null;
 	}
-	
+
 	public static boolean isServer() {
 		return FMLCommonHandler.instance().getEffectiveSide().equals(Side.SERVER);
 	}
+
 	public static boolean isClient() {
 		return !Bukkit.isServer();
 	}
+
 	private static BukkitEventManager manager;
-	
-	public static BukkitEventManager getEventManager(){
+
+	public static BukkitEventManager getEventManager() {
 		return (Bukkit.manager == null ? (Bukkit.manager = new BukkitEventManager()) : Bukkit.manager);
 	}
 
 	public static void initPluginManager() {
-		if(Bukkit.pluginManager == null){
+		if (Bukkit.pluginManager == null) {
 			Bukkit.pluginManager = new BukkitPluginManager();
 		}
 	}
 
 	public static void initCommandManager() {
-		if(Bukkit.commandManager == null){
+		if (Bukkit.commandManager == null) {
 			Bukkit.commandManager = new BukkitCommandManager();
 		}
 	}
@@ -108,36 +115,37 @@ public class Bukkit {
 	public static BukkitCommandManager getCommandManager() {
 		return Bukkit.commandManager;
 	}
+
 	@SideOnly(Side.CLIENT)
-	public static ArrayList<ServerListEntry> getMinecraftMultiplayerServerListEntries(){
+	public static ArrayList<ServerListEntry> getMinecraftMultiplayerServerListEntries() {
 		ArrayList<ServerListEntry> groups = new ArrayList<ServerListEntry>();
-		try{
+		try {
 			Map<String, Object> obj = NBTStreamReader.read(new FileInputStream(new File(Minecraft.getMinecraft().mcDataDir.getAbsolutePath() + "\\servers.dat")), false);
 			String ll = "";
-			for(Object o: obj.values()){
+			for (Object o : obj.values()) {
 				ll += o.toString().replace("\r\n", "").replace("\n", "");
 			}
 			int i = 0;
 			int length = 0;
 			int begin = 0;
 			System.out.println(ll);
-			while(true){
+			while (true) {
 				char current = ll.charAt(i);
-				if(current == '{') {
+				if (current == '{') {
 					begin = i;
-					
-				} else if(current == '}') {
+
+				} else if (current == '}') {
 					groups.add(new ServerListEntry(ll.substring(begin + 1, i).split(", ")));
 				}
 				i++;
-				if(i == ll.length()){
+				if (i == ll.length()) {
 					break;
 				}
 			}
-		} catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
+
 		return groups;
 	}
 }
