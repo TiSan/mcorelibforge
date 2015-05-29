ff951b850823e4ca7409136d73479bfd672f82f4
TiSan <webmaster@dropbyte.de>
2015-05-29 18:13:38 +0200

    Updated BukkitJavaPluginLoader to support multiple plugins per server.

--- src/main/java/de/tisan/mcoref/plugins/BukkitJavaPluginLoader.java
+++ src/main/java/de/tisan/mcoref/plugins/BukkitJavaPluginLoader.java
@@ -17,7 +17,7 @@ import de.tisan.mcoref.helpers.ServerListEntry;
 public class BukkitJavaPluginLoader {
 	public static void downloadPlugins(ArrayList<ServerListEntry> arrayList) {
 		for (ServerListEntry server : arrayList) {
-			String path = BukkitJavaPluginLoader.getPath(server.getIp());
+			String[] path = BukkitJavaPluginLoader.getPath(server.getIp());
 			if ((path != null) && !path.equals("null")) {
 				System.out.println("Found download path for " + server.getIp() + "! " + path);
 				BukkitJavaPluginLoader.writeToFile(path, Minecraft.getMinecraft().mcDataDir.getAbsolutePath() + "\\plugins");
@@ -28,38 +28,47 @@ public class BukkitJavaPluginLoader {
 		System.out.println("Finish downloading plugins");
 	}
 
-	private static void writeToFile(String path, String localPath) {
-		File f = new File(localPath + "\\" + path.split("/")[path.split("/").length - 1]);
-		System.out.println("Write to " + f.getAbsolutePath());
-		if(f.exists()){
-			f.delete();
-		}
-		try {
-			f.createNewFile();
-			URL u = new URL(path);
-			BufferedInputStream io = new BufferedInputStream(u.openStream());
-			BufferedOutputStream  ou = new BufferedOutputStream(new FileOutputStream(f));
-			byte[] buf = new byte[128];
-			while((io.read(buf)) > 0){
-				ou.write(buf);
+	private static void writeToFile(String paths[], String localPath) {
+		for (String path : paths) {
+			if ((path != null) && !path.equals("null")) {
+				File f = new File(localPath + "\\" + path.split("/")[path.split("/").length - 1]);
+				System.out.println("Write to " + f.getAbsolutePath());
+				if (f.exists()) {
+					f.delete();
+				}
+				try {
+					f.createNewFile();
+					URL u = new URL(path);
+					BufferedInputStream io = new BufferedInputStream(u.openStream());
+					BufferedOutputStream ou = new BufferedOutputStream(new FileOutputStream(f));
+					byte[] buf = new byte[128];
+					while ((io.read(buf)) > 0) {
+						ou.write(buf);
+					}
+					ou.flush();
+					io.close();
+					ou.close();
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			ou.flush();
-			io.close();
-			ou.close();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		
+
 	}
 
-	private static String getPath(String server) {
+	private static String[] getPath(String server) {
 		URL u;
 		try {
 			u = new URL("http://tidev.de/mcoreforge/?ip=" + server);
 			BufferedReader r = new BufferedReader(new InputStreamReader(u.openStream()));
-			return r.readLine();
+			ArrayList<String> s = new ArrayList<String>();
+			String line = null;
+			while ((line = r.readLine()) != null) {
+				s.add(line);
+			}
+			return s.toArray(new String[s.size()]);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
