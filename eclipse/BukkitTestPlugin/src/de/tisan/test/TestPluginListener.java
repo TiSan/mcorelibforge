4193b5c48b3b3dfd0ca53ea612c106c89f090b4a
mcNETDev <mcfrek99@gmail.com>
2015-05-30 16:21:13 +0200

    Added Extended Prop

--- eclipse/BukkitTestPlugin/src/de/tisan/test/TestPluginListener.java
+++ eclipse/BukkitTestPlugin/src/de/tisan/test/TestPluginListener.java
@@ -3,12 +3,27 @@ package de.tisan.test;
 import de.tisan.mcoref.events.core.BukkitEventHandler;
 import de.tisan.mcoref.events.properties.BukkitBlockInteractByPlayerEvent;
 import de.tisan.mcoref.events.properties.BukkitListener;
+import de.tisan.mcoref.events.properties.EntityConstructEvent;
+import de.tisan.mcoref.events.properties.EntityJoinWorldEvent;
+import de.tisan.mcoref.plugins.entities.BukkitEntity;
 
-public class TestPluginListener implements BukkitListener{
-	
-	
+public class TestPluginListener implements BukkitListener {
+
 	@BukkitEventHandler
-	public void onBling(BukkitBlockInteractByPlayerEvent event){
-		
+	public void onBling(BukkitBlockInteractByPlayerEvent event) {
+
+	}
+
+	@BukkitEventHandler
+	public void onEntityContruct(EntityConstructEvent event) {
+		BukkitEntity ent = event.getEntity();
+		ent.registerExtendedProperties("Test", new ManaProp());
+	}
+
+	@BukkitEventHandler
+	public void onJump(EntityJoinWorldEvent event) {
+		BukkitEntity ent = event.getEntity();
+		ManaProp mana = (ManaProp) ent.getExtendedProperties("Test");
+		System.out.println("Mana = " + mana.mana);
 	}
 }
